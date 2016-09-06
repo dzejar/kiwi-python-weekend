248a2fe11311fc9fef6a67176593f67fd10aae19
Jan Richter <dzejar@dzejar.net>
2016-09-07 00:28:53 +0200

    separate class for connection search

--- find_combinations.py
+++ find_combinations.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-import copy
 import csv
 import sys
 from datetime import datetime, timedelta
 
 itinerary_min_length = 2
-itinerary_max_length = 16
+itinerary_max_length = 32
 connection_min_delta = timedelta(hours=1)
 connection_max_delta = timedelta(hours=4)
 
 
 class Flight(object):
     """Flight representing class"""
-
     source = None
     destination = None
     departure = None
@@ -29,9 +27,6 @@ class Flight(object):
     def __repr__(self):
         return "[%r] %r -> %r @ %r" % (self.flight_number, self.source,
                                        self.destination, self.departure)
-
-    def __lt__(self, other):
-        return self.arrival < other.departure
 
     def connects(self, other):
         """Check if other flight can be connected to this one"""
@@ -50,25 +45,40 @@ class Flight(object):
                self.destination == other.destination
 
 
-def find_connections(flight_list, flight, itinerary, result_list):
-    itinerary = copy.copy(itinerary)
-    itinerary.append(flight)
-    if len(itinerary) >= itinerary_min_length:
-        # print(','.join([f.flight_number for f in itinerary]))
-        result_list.append(itinerary)
-    if len(itinerary) >= itinerary_max_length:
-        return # avoid unlimited recursion
-    for i, f2 in enumerate(flight_list):
-        if flight.connects(f2) and not any([f2.is_same(r) for r in itinerary]):
-            find_connections(flight_list[i+1:], f2, itinerary, result_list)
-        elif f2.departure - flight.arrival > connection_max_delta:
-            # if this was too late, all following will be late either, skip
-            return
+class TaskSolver(object):
+    """Task solving class"""
+    def __init__(self, flight_list):
+        self._flights = flight_list
+        # sort flights by departure time to optimize searches later
+        flight_list.sort(key=lambda o: o.departure)
+        self._results = []
+
+    def _find_connections(self, flight_list, flight, itinerary):
+        """recursive connection search"""
+        itinerary = itinerary[:]  # copy the list, don't append to original
+        itinerary.append(flight)
+        if len(itinerary) >= itinerary_min_length:
+            self._results.append(itinerary)
+        if len(itinerary) >= itinerary_max_length:
+            return  # avoid unlimited recursion
+        for i, f2 in enumerate(flight_list):
+            if flight.connects(f2) and not any([f2.is_same(r) for r in itinerary]):
+                self._find_connections(flight_list[i + 1:], f2, itinerary)
+            elif f2.departure - flight.arrival > connection_max_delta:
+                # if this was too late, all following will be late either, prune
+                return
+
+    def solve(self):
+        for i, f in enumerate(self._flights):
+            self._find_connections(flights[i + 1:], f, [])
+
+    def get_results(self):
+        return self._results
 
 
 if __name__ == '__main__':
-    # input_data = csv.DictReader(iter(sys.stdin.readline, ''))
-    input_data = csv.DictReader(open('input.csv'))
+    input_data = csv.DictReader(iter(sys.stdin.readline, ''))
+    # input_data = csv.DictReader(open('input.csv'))
     flights = []
     for row in input_data:
         assert all(key in row for key in ('source', 'destination', 'departure',
@@ -77,12 +87,9 @@ if __name__ == '__main__':
         f = Flight(**row)
         flights.append(f)
 
-    # sort flights by departure time to optimize searches later
-    flights.sort(key=lambda o:o.departure)
+    task_solver = TaskSolver(flights)
+    task_solver.solve()
 
-    results = []
-    for i, f in enumerate(flights):
-        find_connections(flights[i+1:], f, [], results)
-
-    for r in results:
+    for r in task_solver.get_results():
         print(','.join([f.flight_number for f in r]))
+
